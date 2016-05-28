5a1571525bc3def900aaea0f7c0a85289d6ff345
Brian Chirgwin <brian@chirgwin.net>
2016-05-28 15:14:46 -0500

    added client request sample

---
+++ server02-users-getUser/client.js
@@ -0,0 +1,26 @@
+var request = require('request');
+
+var baseUrl = 'http://localhost:8080';
+
+var getUsers = function () {
+    request(baseUrl + '/api/users', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('request: GET http://localhost:8008/api/users');
+            console.log(JSON.parse(body))
+        }
+    });
+};
+
+var getUser = function (id) {
+    request(baseUrl + '/api/users/' + id, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('request: GET http://localhost:8008/api/users/5');
+            console.log(JSON.parse(body));
+        }
+    });
+};
+
+getUsers();
+
+getUser(5);
+
---
+++ server04-users-updateUser/client.js
@@ -0,0 +1,50 @@
+var request = require('request');
+
+var baseUrl = 'http://localhost:8080';
+
+var getUsers = function () {
+    request(baseUrl + '/api/users', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('request: GET http://localhost:8008/api/users');
+            console.log(JSON.parse(body))
+        }
+    });
+};
+
+var getUser = function (id) {
+    request(baseUrl + '/api/users/' + id, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('request: GET http://localhost:8008/api/users/5');
+            console.log(JSON.parse(body));
+        }
+    });
+};
+
+var addUser = function (name) {
+    request.post(baseUrl + '/api/users', {"form": {"name": name}}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('request: POST http://localhost:8008/api/users');
+            console.log(JSON.parse(body));
+        }
+    });
+};
+
+var updateUser = function (id, name) {
+    request.post(baseUrl + '/api/users/' + id, {"form": {"name": name}}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('request: POST http://localhost:8008/api/users/7', name);
+            console.log(JSON.parse(body));
+        }
+        ;
+    });
+}
+
+getUsers();
+
+getUser(5);
+
+addUser('Snow White');
+getUsers();
+
+updateUser(7, 'Queen');
+getUsers();
